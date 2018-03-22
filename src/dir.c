4cc232d9feb38f00c2a99d4e3d353f6ee3449906
itsnamgyu <itsnamgyu@gmail.com>
2018-03-22 17:54:13 +0900

    implement & pseudo-test dir command

---
+++ src/dir.c
@@ -0,0 +1,33 @@
+#include "dir.h"
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+int main(void) {
+	fprint_dir(stdout);
+}
+
+void fprint_dir(FILE *out) {
+	struct dirent *entry;
+	DIR *directory = opendir(".");
+
+	while ((entry = readdir(directory))) {
+		char *filename = entry->d_name;
+		struct stat buffer;
+		stat(filename, &buffer);
+
+		if (filename[0] == '.') continue;
+
+		if (entry->d_type == DT_DIR) {
+			fprintf(out, "%s\t", filename);
+			continue;
+		}
+
+		fprintf(out, "%s", filename);
+		if (entry->d_type == DT_DIR) fprintf(out, "\\");
+		else if (buffer.st_mode & S_IXUSR) fprintf(out, "*");
+		fprintf(out, "\t");
+	}
+
+	fprintf(out, "\n");
+}
---
+++ src/dir.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+void fprint_dir(FILE *out);
