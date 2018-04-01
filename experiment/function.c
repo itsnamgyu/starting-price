f045451a4e17d484e14516b37e4cf9de9bdcb86b
itsnamgyu <itsnamgyu@gmail.com>
2018-04-01 18:58:47 +0900

    experiment incompatible function signatures
    When you assign a function pointer with signature A to a function
    pointer variable with signature B and call it, then the extra arguments
    will be ignored (if there are more arguments), and the missing arguments
    will be left NULL-initialized (if there are less arguments, but this is
    probably undefined behavior)

---
+++ experiment/function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sum(int a, int b) {
+	return a + b;
+}
+
+int main(void) {
+	int (*f)(int);
+	int (*g)(int, int, int);
+
+	f = sum;
+	g = sum;
+
+	int ho = f(10);
+	printf("%d\n", ho);
+
+	ho = g(10, 20, 30);
+	printf("%d\n", ho);
+
+
+	return 0;
+}
