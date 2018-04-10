a042470568a897a25ec6246d5dc5b03f1269dc50
itsnamgyu <itsnamgyu@gmail.com>
2018-04-10 17:29:03 +0900

    comment symbol.h LIEK A BOSS
    Techincally this isn't commit inflation because the previous 'shameful
    commit' was literally thousands of lines of code

--- src/symbol.h
+++ src/symbol.h
@@ -1,3 +1,13 @@
+/*
+ * symbol.h
+ *
+ * This module provides the SymbolDict type that you can use to store and
+ * retrive symbols and their corresponding instruction addresses.
+ *
+ * The functions are just wrapper functions for the generic dict type, so
+ * they are pretty self explanatory.
+ */
+
 #ifndef SYMBOL_H
 #define SYMBOL_H
 #include "generic_dict.h"
@@ -23,6 +33,10 @@ int find_symbol_address(SymbolDict *dict, char *string, unsigned int *address);
 void free_symbol_dict(SymbolDict *dict);
 
 void fprint_symbols(FILE *out, SymbolDict *dict);
+/*
+ * Prints the symbols in *dict* in descending order, based on the string of
+ * the symbol.
+ */
 
 #define new_symbol_table new_symbol_dict
 #define add_to_symbol_table add_to_symbol_dict
