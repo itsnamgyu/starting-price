2a0e7d5dd4bd1a3764d44efeea81a6adfe0858cb
itsnamgyu <itsnamgyu@gmail.com>
2018-04-28 14:55:13 +0900

    implement and test external symbol table module

---
+++ src/estab.c
@@ -0,0 +1,87 @@
+#define ESTAB_C
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "estab.h"
+
+static void add_symbol_to_esdict(Dict *dict, const char *symbol, int address);
+// Add symbol and address to dictionary in estab, abiding to the generic_dict interface.
+
+#define ESTAB_BUCKET_SIZE 20
+Estab *new_estab() {
+	Estab *estab = malloc(sizeof(Estab));
+	
+	for (int i = 0; i < REFERENCE_ARRAY_SIZE; ++i)
+		estab->list[i] = -1;
+	estab->dict = new_dict(ESTAB_BUCKET_SIZE);
+
+	return estab;
+}
+
+void free_estab(Estab *estab) {
+	free_dict(estab->dict, free);
+	free(estab);
+}
+
+bool add_to_estab(Estab *estab, const char *symbol, int reference_number, int address) {
+	bool add_symbol = (symbol != NULL);
+	bool add_reference = (reference_number != -1);
+
+	assert(add_symbol || add_reference && "must specify symbol or reference");
+	// should have been checked during OBJ read
+	assert(address >= 0 && "address must be non-negative");
+	// should have been checked during OBJ read (since it's hex?)
+	
+	if (add_symbol && dict_contains(estab->dict, symbol)) return false;
+	if (add_reference && estab->list[reference_number] != -1) return false;
+
+	if (add_symbol) add_symbol_to_esdict(estab->dict, symbol, address);
+	if (add_reference) estab->list[reference_number] = address;
+
+	return true;
+}
+
+int find_from_estab_by_symbol(Estab *estab, const char *symbol) {
+	int *value;
+
+	if (!find_from_dict(estab->dict, symbol, (void**) &value)) return -1;
+	return *value;
+}
+
+int find_from_estab_by_reference_number(Estab *estab, int reference_number) {
+	assert(reference_number >= 0 && "reference number must be non-negative");
+	// should have been checked during OBJ read
+	assert(reference_number < REFERENCE_ARRAY_SIZE && "reference number can't be too big");
+	// should have been checked during OBJ read
+	
+	return estab->list[reference_number];
+}
+
+
+static void add_symbol_to_esdict(Dict *dict, const char *symbol, int address) {
+	int *value = malloc(sizeof(int));
+	*value = address;
+	add_to_dict(dict, symbol, (void*) value);
+}
+
+#ifdef TEST
+int main(void) {
+	Estab *estab = new_estab();
+
+	assert(add_to_estab(estab, "Hello", 1, 1024) == 1);
+	assert(add_to_estab(estab, "Ho", 2, 2048) == 1);
+	assert(find_from_estab_by_symbol(estab, "Hello") == 1024);
+	assert(find_from_estab_by_reference_number(estab, 1) == 1024);
+	assert(find_from_estab_by_symbol(estab, "Ho") == 2048);
+	assert(find_from_estab_by_reference_number(estab, 2) == 2048);
+	assert(find_from_estab_by_symbol(estab, "Hollo") == -1);
+	assert(find_from_estab_by_reference_number(estab, 3) == -1);
+
+	printf("----------------------------------------\n");
+	printf("Automatic tests successful!\n");
+
+	return 0;
+}
+#endif
---
+++ src/estab.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <stdbool.h>
+
+#include "generic_dict.h"
+
+#define REFERENCE_ARRAY_SIZE 10000
+typedef struct _Estab {
+	int list[REFERENCE_ARRAY_SIZE];
+	Dict *dict;
+} Estab;
+/*
+ * External symbol table which is searchable by symbol or reference.
+ *
+ * list:
+ * Saves the address of symbols in an array with the index corresponds to the reference number
+ * assigned to the symbol. When for reference numbers that have not been assigned, the address
+ * is set to -1. Naturally, the array is initialized with -1.
+ *
+ * dictionary:
+ * Saves the address of symbols in a dictionary of <symbol-address> pairs.
+ */
+
+Estab *new_estab();
+/*
+ * Initialized a new estab and return a reference.
+ */
+
+void free_estab(Estab *estab);
+/*
+ * Deinitialize estab.
+ */
+
+bool add_to_estab(Estab *estab, const char *symbol, int reference_number, int address);
+/*
+ * Add symbol with optional symbol, reference number and the target address.
+ *
+ * Arguments:
+ * reference_number = -1: NULL
+ *
+ * Note that at least one of symbol or reference number must be specified.
+ */
+
+int find_from_estab_by_symbol(Estab *estab, const char *symbol);
+/*
+ * Find address from estab using original symbol string
+ *
+ * Return Values:
+ * -1: not found
+ * default: the address that was found
+ */
+
+int find_from_estab_by_reference_number(Estab *estab, int reference_number);
+/*
+ * Find address from estab using the reference number.
+ *
+ * Return Values:
+ * -1: not found
+ * default: the address that was found
+ */
